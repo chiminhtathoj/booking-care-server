f5568cc9b2e4a663593d574ae810a4050d3ec75a
chiminhtathoj <vdlong2009@gmail.com>
2022-04-09 15:29:51 +0700

    add feature read in crud

--- src/route/web.js
+++ src/route/web.js
@@ -8,6 +8,7 @@ const initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage)
     router.get("/crud", homeController.getCrud)
     router.post("/post-crud", homeController.postCrud)
+    router.get("/displayCrud", homeController.displayCrud)
     return app.use("/", router)
 }
 
